c887aaa819010fa05c4579e0f9f2916ff459fa08
admin <pedro.caridade@innowave.tech>
2021-02-22 18:10:13 +0000

    lesson 15

--- app/src/main/java/com/example/smackappp/Controller/LoginActivity.kt
+++ app/src/main/java/com/example/smackappp/Controller/LoginActivity.kt
@@ -5,6 +5,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import com.example.smackappp.R
+import com.example.smackappp.Services.AuthService
+import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -13,7 +15,20 @@ class LoginActivity : AppCompatActivity() {
     }
 
     fun loginLoginBtnClicked(view: View) {
+        val email = loginEmailTxt.text.toString()
+        val password = loginPasswordTxt.text.toString()
 
+        AuthService.loginUser(this, email, password) { loginSuccess ->
+            if (loginSuccess) {
+                AuthService.findUserByEmail(this) { findSuccess ->
+                    if (findSuccess) {
+                        finish()
+                    }
+
+                }
+            }
+
+        }
     }
 
     fun loginCreateUserBtnClicked(view: View) {
